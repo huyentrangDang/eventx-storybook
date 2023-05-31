eaa916a3a5f1dc0d10e2bd4337c29f11e2d76055
Thuy Ngoc <ngoc.maithuy@ncc.asia>
2023-05-31 08:27:12 +0700

    update component avatar

--- src/stories/components/avatar/Avatar.stories.js
+++ src/stories/components/avatar/Avatar.stories.js
@@ -1,4 +1,7 @@
 import { Avatar } from './Avatar';
+import { AvatarGroup } from './AvatarGroup';
+import { AvatarLabelGroup } from './AvatarLabelGroup';
+import { AvatarProfilePhoto } from './AvatarProfilePhoto';
 
 export default {
   title: 'Avatar/Avatar',
@@ -16,3 +19,78 @@ avatars.args = {
   statusIcon: 'false',
 };
 avatars.argTypes = {};
+
+const Avatar_Group = (args) => <AvatarGroup {...args} />;
+export const avatar_group = Avatar_Group.bind({});
+avatar_group.args = {
+  size: 'xs',
+  moreUser: 'true',
+  addMore: 'true',
+};
+avatar_group.argTypes = {
+  moreUser: { table: { disable: true } },
+  addMore: { table: { disable: true } },
+  text: { table: { disable: true } },
+  placeholder: { table: { disable: true } },
+  state: { table: { disable: true } },
+  statusIcon: { table: { disable: true } },
+  size: {
+    control: 'radio',
+    options: ['xs', 'sm', 'md'],
+  },
+};
+
+const Avatar_Label_Group = (args) => <AvatarLabelGroup {...args} />;
+export const avatar_label_group = Avatar_Label_Group.bind({});
+avatar_label_group.args = {
+  size: 'sm',
+  statusIcon: 'false',
+  state: 'default',
+  text: 'Olivia Rhye',
+  supportText: 'olivia@untitledui.com',
+};
+avatar_label_group.argTypes = {
+  placeholder: { table: { disable: true } },
+  text: { control: 'text' },
+  state: {
+    control: 'radio',
+    options: ['default', 'hover', 'focused'],
+  },
+  statusIcon: {
+    control: 'radio',
+    options: ['false', 'onlineIndicator', 'company', 'verified'],
+  },
+  size: {
+    control: 'radio',
+    options: ['sm', 'md', 'lg', 'xl'],
+  },
+};
+
+const Avatar_Profile_Photo = (args) => <AvatarProfilePhoto {...args} />;
+export const avatar_profile_photo = Avatar_Profile_Photo.bind({});
+avatar_profile_photo.args = {
+  text: 'false',
+  size: 'sm',
+  placeholder: 'false',
+  verified: true,
+};
+avatar_profile_photo.argTypes = {
+  state: { table: { disable: true } },
+  statusIcon: { table: { disable: true } },
+  placeholder: {
+    control: 'radio',
+    options: ['true', 'false'],
+  },
+  text: {
+    control: 'radio',
+    options: ['true', 'false'],
+  },
+  verified: {
+    control: 'boolean',
+    options: [true, false],
+  },
+  size: {
+    control: 'radio',
+    options: ['sm', 'md', 'lg'],
+  },
+};
